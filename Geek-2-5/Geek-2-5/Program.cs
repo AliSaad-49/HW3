67708c134c1634cba14bc8eb4009e694d217a6d8
AliSaad-49 <aliedan94@mail.ru>
2021-03-01 21:28:34 +0300

    homework2

---
+++ Geek-2-5/Geek-2-5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Geek_2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int tem = 0;
+            string mon = null;
+
+            
+            for (int i = 0; i <= 12; i++)
+            {
+                
+
+              
+
+                Console.WriteLine(" Введите месяц Зимней природы :");
+                mon = Console.ReadLine();
+                Console.WriteLine("Ввидите Текщую Тембиратору");
+                tem = Convert.ToInt32( Console.ReadLine());
+
+
+                if (tem > 0)
+                {
+                    Console.WriteLine("Дождливая зима");
+                    break;
+                }
+
+                else
+                    Console.WriteLine("Не Правильный Вывод!!,Попробуйте ещё раз.");
+                
+                Console.ReadLine();
+                
+
+                
+
+                  
+                
+                  
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Geek-2-5/homework2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mintemp, maxtemp;
+            double middletemp;
+
+            Console.WriteLine("please enter the maximum temprture:");
+            maxtemp = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter the munimum temprture:");
+            mintemp = Convert.ToInt32(Console.ReadLine());
+
+            middletemp = (mintemp + maxtemp) /2;
+
+            Console.WriteLine(" The middle tempruture is " + mintemp +" degree ");
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
