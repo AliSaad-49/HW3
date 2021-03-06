67196baded7f3ca74e5aad3217909d6d7386829a
AliSaad-49 <aliedan94@mail.ru>
2021-03-06 23:00:08 +0300

    hw3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[,] mass = { { "Ali", "Ali@gmail.com" } , { "Ahmed", "Ahmed@gmail.com" },{"maxim" , "maxim@gmail.com" }, { "Alex" , "Alex@gmail.com"},{ "masha", "masha@gmail.com"} };
+
+            Console.WriteLine("phone contacts:");
+            for (int i = 0; i < mass.GetLength(0); i++) 
+            {
+                Console.WriteLine($"{mass[i, 0]}: {mass[i, 1]}");
+            }
+           
+              
+
+           
+        }
+    }
+}
